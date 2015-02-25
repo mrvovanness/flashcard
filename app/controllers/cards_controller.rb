c52869265511de351d82b74fc02139f6f283c125
mrvovanness <mrvovanness@gmail.com>
2015-02-25 16:20:44 +0500

    Before revision

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,9 @@
+class CardsController < ApplicationController
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    @cards = Card.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  resources :cards
+
   get "home" => "home#index"
-  # You can have the root of your site routed with "root"
   root 'home#index'
 
   # Example of regular route:
