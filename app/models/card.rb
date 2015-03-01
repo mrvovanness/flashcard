034c1ecce9c6d5b97bfd87ed8a2d862296cec1b2
mrvovanness <mrvovanness@gmail.com>
2015-03-01 18:28:26 +0500

    CRUD, partials, validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,15 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  validate :the_text_cannot_be_the_same
+
+  def the_text_cannot_be_the_same
+    if original_text == translated_text
+      errors.add(:translated_text, "не похоже на перевод")
+    end
+  end
+
+  before_validation do
+    self.review_date = Date.today + 3.days
+  end
 end
