bbc41a14e20efcf94a4d47aaa9324802f5a0cd10
mrvovanness <mrvovanness@gmail.com>
2015-03-13 22:41:00 +0500

    Add 2th model and association

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :card do
     original_text "Mantle"
     translated_text "Мантия"
+    user_id 1
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 describe Card do
+  let! (:card) { create(:card) }
+
   it "check matching words" do
-    card = Card.create(original_text: "feature", translated_text: "фишка")
-    expect(card.check_translation("feature")).to be true
+    expect(card.check_translation("Mantle")).to be true
   end
 
   it "check mathing words regardless language and case" do
-    card = Card.create(original_text: "Кухня", translated_text: "Kitchen")
-    expect(card.check_translation("куХНя")).to be true
+    expect(card.check_translation("mANtlE")).to be true
   end
 
   it "check unmatching words" do
-    card = Card.create(original_text: "feature", translated_text: "фишка")
     expect(card.check_translation("future")).to be false
   end
 
@@ -22,17 +21,10 @@ describe Card do
   end
 
   it "pass nonequal words" do
-    card = Card.create(original_text: "Fall", translated_text: "Осень")
     expect(card.errors.any?).to be false
   end
 
-  it "don't pass equal words" do
-    card = Card.create(original_text: "Fall", translated_text: "Fall")
-    expect(card.errors.any?).to be true
-  end
-
-  context "deals with database" do
-    let! (:card) { create(:card) }
+  context "deals with updating attributes" do
     before do
       card.update_attribute(:review_date, Date.today - 1.day)
     end
@@ -41,5 +33,10 @@ describe Card do
       succ_card = Card.pending.first
       expect { succ_card.check_translation(succ_card.original_text) }.to change { Card.pending.count }.by(-1)
     end
+  end
+
+   it "don't pass equal words" do
+    card = Card.create(original_text: "Fall", translated_text: "Fall")
+    expect(card.errors.any?).to be true
   end
 end
