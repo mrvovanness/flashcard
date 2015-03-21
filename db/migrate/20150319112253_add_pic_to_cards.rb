b4bc6f7bf8d9d21bf975d8e7dfccfc355b969f6a
mrvovanness <mrvovanness@gmail.com>
2015-03-22 00:51:35 +0500

    Delete some migrations

--- db/migrate/20150319112253_add_pic_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddPicToCards < ActiveRecord::Migration
-  def change
-    add_column :cards, :card_pic, :string
-  end
-end
--- db/migrate/20150320161523_change_column_name_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameToCards < ActiveRecord::Migration
-  def change
-    rename_column :cards, :card_picture, :visual
-  end
-end
