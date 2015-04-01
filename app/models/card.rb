955d8e5b9a0e49fb3f52b26ede9c1dc87fafca82
mrvovanness <mrvovanness@gmail.com>
2015-04-01 22:08:49 +0500

    Add Leitner system

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ActiveRecord::Base
   belongs_to :deck
   mount_uploader :picture, PictureUploader
 
-  before_validation :shift_date, on: :create
+  before_validation :set_date, on: :create
   
   validates :original_text,
             :translated_text,
@@ -12,7 +12,7 @@ class Card < ActiveRecord::Base
 
   validate :the_text_cannot_be_the_same
 
-  scope :pending, -> { where('review_date <=?', Date.today).order("RANDOM()") }
+  scope :pending, -> { where('review_date <=?', DateTime.now).order("RANDOM()") }
 
   def the_text_cannot_be_the_same
     if original_text == translated_text
@@ -20,15 +20,41 @@ class Card < ActiveRecord::Base
     end
   end
 
-  def shift_date
-    self.review_date = Date.today + 3.days
+  def set_date
+    self.review_date = DateTime.now
   end
 
   def check_translation(user_translation)
     if user_translation.mb_chars.downcase.strip == original_text.mb_chars.downcase.strip
-      update_attributes(review_date: Date.today + 3.days)
+      update_attributes(review_date: review_shift, box: box_shift, fail_count: 0)
     else
+      update_attributes(fail_count: fail_count + 1)
+      if fail_count == 3
+        update_attributes(review_date: DateTime.now + 12.hours, box: 1, fail_count: 0)
+      end
       return false
+    end
+  end
+
+  def review_shift
+    if box == 0
+      DateTime.now + 12.hours
+    elsif box == 1
+      DateTime.now + 3.days
+    elsif box == 2
+      DateTime.now + 1.weeks
+    elsif box == 3
+      DateTime.now + 2.weeks
+    elsif box == 4
+      DateTime.now + 1.months
+    end
+  end
+
+  def box_shift
+    if box < 4
+      box + 1
+    else
+      4
     end
   end
 end
